4e8bf412ea5853a956f1cf5da2c93e421347a92a
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-09 22:56:59 +0200

    Atualizar o DataContext.cs

--- Data/DataContext.cs
+++ Data/DataContext.cs
@@ -8,7 +8,7 @@ public class DataContext : DbContext
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
         var version = new MySqlServerVersion(new Version(8, 0, 29));
-        optionsBuilder.UseMySql("server=ec2-52-90-110-80.compute-1.amazonaws.com;database=database;user=root;password=bruno1234;", version);   
+        optionsBuilder.UseMySql("server=localhost;database=database;user=root;password=bruno1234;", version);   
     }
     public DbSet<User> Users { get; set; }
 }
