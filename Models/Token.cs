52fe853f9171499bc205b4e93ea62a33dffc1875
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-10 09:38:28 +0200

    Atualizar o Token.cs

--- Models/Token.cs
+++ Models/Token.cs
@@ -2,6 +2,8 @@ namespace micropay.Models;
 
 public class Token
 {
+    public User User { get; set; }
+    public Guid UserId { get; set; }
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Type { get; set; } = string.Empty;
