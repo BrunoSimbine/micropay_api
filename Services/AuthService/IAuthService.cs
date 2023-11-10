db9a3d74173d9b6386bf1ab3aea86b32857f77d4
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-10 09:30:04 +0200

    Atualizar o IAuthService.cs

--- Services/AuthService/IAuthService.cs
+++ Services/AuthService/IAuthService.cs
@@ -1,1 +1,10 @@
-h
+using micropay.Models;
+
+namespace micropay.Services.AuthService;
+
+public interface IAuthService
+{
+    void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+    bool VerifyPasswordHash(string password, byte[] passwordHash, byte[] passwordSalt);
+    string CreateToken(User user);
+}
