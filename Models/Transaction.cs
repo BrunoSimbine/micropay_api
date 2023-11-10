704068b9bf3cd9083827d9249324e82ab4024bec
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-10 09:39:30 +0200

    Atualizar o Transaction.cs

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -2,6 +2,8 @@ namespace micropay.Models;
 
 public class Transaction
 {
+    public Token Token { get; set; }
+    public Guid TokenId { get; set; }
     public Guid Id { get; set; }
     public DateTime Created { get; set; } = string.Empty;
     public double Amount { get; set; }
