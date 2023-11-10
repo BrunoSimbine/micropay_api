2f7c91a203b34181131134680b4703797ca7c4a5
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-10 10:57:39 +0200

    Atualizar o Transaction.cs

--- Models/Transaction.cs
+++ Models/Transaction.cs
@@ -5,7 +5,7 @@ public class Transaction
     public Token Token { get; set; }
     public Guid TokenId { get; set; }
     public Guid Id { get; set; }
-    public DateTime Created { get; set; } = string.Empty;
+    public DateTime Created { get; set; } = DateTime.Now;
     public double Amount { get; set; }
     public string FromType { get; set; } = string.Empty;
     public string FromAccount { get; set; } = string.Empty;
