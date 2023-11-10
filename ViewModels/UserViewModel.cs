0415552d402dc370e45235be4815c4b7b6062b1e
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-10 09:44:39 +0200

    Atualizar o UserViewModel.cs

---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,8 @@
+namespace micropay.ViewModels;
+
+public class UserViewModel
+{
+    public Guid Id { get; set; }
+    public string? Name { get; set; } 
+    public string? Surname { get; set; } 
+}
