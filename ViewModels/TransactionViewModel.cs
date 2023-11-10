24df7aec5ddb973ccb7117d555e3d79b2139143a
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-10 09:51:48 +0200

    Atualizar o TransactionViewModel.cs

--- ViewModels/TransactionViewModel.cs
+++ ViewModels/TransactionViewModel.cs
@@ -1,1 +1,9 @@
-g
+namespace micropay.ViewModels;
+
+public class TransactionViewModel
+{
+    public Guid Id { get; set; }
+    public double Amount { get; set; }
+    public string FromType { get; set; } = string.Empty;
+    public string FromAccount { get; set; } = string.Empty;
+}
