4d169d5f3c5590aa14693c7eb53b33b36ed8457f
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-19 22:12:33 +0200

    Atualizar o ITokenService.cs

--- Services/TokenService/ITokenService.cs
+++ Services/TokenService/ITokenService.cs
@@ -1,1 +1,10 @@
-ns
+using micropay.Dto;
+using micropay.Models;
+using micropay.ViewModels;
+
+namespace micropay.Services.TokenService;
+
+public interface ITokenService
+{
+    string Create(TokenDto tokenDto);
+}
