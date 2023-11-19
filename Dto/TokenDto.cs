515c9b2928c69981b6062fc6afcd5be3e535bbe5
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-19 21:20:51 +0200

    Atualizar o TokenDto.cs

---
+++ Dto/TokenDto.cs
@@ -0,0 +1,8 @@
+namespace micropay.Dto;
+
+public class TokenDto
+{
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Account { get; set; } = string.Empty;
+}
