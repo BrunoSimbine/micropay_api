2465d5a8af523a48c9eb36b7726f90bde641765a
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-19 21:17:03 +0200

    Atualizar o TokenService.cs

--- Services/TokenService/TokenService.cs
+++ Services/TokenService/TokenService.cs
@@ -1,1 +1,49 @@
-v
+using System.Security.Claims;
+using micropay.Dto;
+using micropay.Models;
+using micropay.Services.AuthService;
+using micropay.ViewModels;
+
+namespace micropay.Services.TokenService;
+
+public class TokenService : IUserService
+{
+    private readonly IHttpContextAccessor _accessor;
+    private readonly IAuthService _authService;
+
+    public TokenService(IHttpContextAccessor accessor, IAuthService authService)
+    {
+        _accessor = accessor;
+        _authService = authService;
+    }
+    public Guid? GetId()
+    {
+        var id = _accessor.HttpContext?.User.FindFirstValue(ClaimTypes.Sid);
+        return Guid.Parse(id!);
+    }
+
+    public List<UserViewModel> GetUsers(List<User> users)
+    {
+        return users.Select(user => new UserViewModel
+        {
+            Id = user.Id,
+            Name = user.Name,
+            Surname = user.Surname
+        }).ToList();
+    }
+
+    public User Create(UserDto userDto)
+    {
+        _authService.CreatePasswordHash(userDto.Password, out byte[] passwordHash, out byte[] passwordSalt);
+        var user = new User()
+        {
+            Name = userDto.Name,
+            Surname = userDto.Surname,
+            Phone = userDto.Phone
+        };
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+
+        return user;
+    }
+}
