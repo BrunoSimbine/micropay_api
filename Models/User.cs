1ea02622b25e220a024799bc75e3d9dd62f5948c
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-22 11:39:02 +0200

    Atualizar o User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -6,6 +6,7 @@ public class User
     public string Name { get; set; } = string.Empty;
     public string Surname { get; set; } = string.Empty;
     public string Role { get; set; } = "user";
+    [Unique]
     public string Phone { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
