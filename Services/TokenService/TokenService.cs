23f5f518b351cd557f0b1d68bc4b9ed75637eefb
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-22 08:14:54 +0200

    Atualizar o TokenService.cs

--- Services/TokenService/TokenService.cs
+++ Services/TokenService/TokenService.cs
@@ -39,17 +39,17 @@ public class TokenService : ITokenService
         }).ToListAsync();
     }
 
-    public async Task<string> Create(TokenDto tokenDto)
+    public async Task<string> Create(TokenDto tokenDto, Guid id)
     {
         //var id = GetId();
-        //var user = _context.Users.FirstOrDefault(e => e.Id == id);
+        var user = _context.Users.FirstOrDefault(e => e.Id == id);
         var token = new Token()
         {
             Name = tokenDto.Name,
             Type = tokenDto.Type,
             Account = tokenDto.Account
         };
-        //token.User = user;
+        token.User = user;
         _context.Tokens.Add(token);
         await _context.SaveChangesAsync();
 
