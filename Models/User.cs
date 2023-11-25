88fc205d16f66aedceba4bdbd07ad0c0484ecec4
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-25 11:23:45 +0200

    Atualizar o User.cs

--- Models/User.cs
+++ Models/User.cs
@@ -2,13 +2,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace micropay.Models;
 
+[nameof(Phone), IsUnique = true]
 public class User
 {
     public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Surname { get; set; } = string.Empty;
     public string Role { get; set; } = "user";
-    [Index(IsUnique = true)]
     public string Phone { get; set; } = string.Empty;
     public byte[] PasswordHash { get; set; }
     public byte[] PasswordSalt { get; set; }
