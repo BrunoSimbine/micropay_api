e83945305df5defe3a15b863588f407207dc0a25
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-28 16:27:38 +0200

    Atualizar o TransactionController.cs

---
+++ Controllers/TransactionController.cs
@@ -0,0 +1,48 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using micropay.Data;
+using micropay.Dto;
+using micropay.Models;
+using micropay.ViewModels;
+using micropay.Services.AuthService;
+using micropay.Services.TokenService;
+using micropay.Services.TransactionService;
+
+namespace micropay.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class TransactionController : ControllerBase
+{
+    private readonly DataContext _context;
+    private readonly ITransactionService _transactionService;
+    public TransactionController(DataContext context, ITransactionService transactionService)
+    {
+        _transactionService = transactionService;
+        _context = context;
+    }
+
+    [HttpGet, Authorize]
+    [Route("get")]
+    public async Task<ActionResult<TransactionViewModel>> GetAll([FromQuery] Guid TokenId)
+    {
+        var token = await _context.Tokens.FirstOrDefaultAsync(e => e.Id == tokenId);
+        var transactions = await _transactionService.GetTransactions(token);
+        return Ok(transactions);
+    }
+
+    [HttpPost, Authorize]
+    [Route("create")]
+    public async Task<ActionResult<string>> Create(TransactionDto transactionDto)
+    {
+        var result = await _transactionService.Create(transactionDto);
+        return Ok(result);
+    }
+
+
+}
