7fc4b93bd8151579d3906261eba13e6b3ce2f4f2
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-28 15:15:33 +0200

    Atualizar o TransactionDto.cs

--- Dto/TransactionDto.cs
+++ Dto/TransactionDto.cs
@@ -1,1 +1,8 @@
-b
+namespace micropay.Dto;
+
+public class TransactionDto
+{
+    public string Amount { get; set; } =  string.Empty;
+    public string TokenId { get; set; } = string.Empty;
+    public string Client { get; set; } = string.Empty;
+}
