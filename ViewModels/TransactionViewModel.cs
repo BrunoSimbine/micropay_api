a1ef1fc63dc4d9eafc1994ac7838726ff6b090b6
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-28 16:37:34 +0200

    Atualizar o TransactionViewModel.cs

--- ViewModels/TransactionViewModel.cs
+++ ViewModels/TransactionViewModel.cs
@@ -4,8 +4,6 @@ public class TransactionViewModel
 {
     public Guid Id { get; set; }
     public double Amount { get; set; }
-    public string Type { get; set; } = string.Empty;
-    public string Account { get; set; } = string.Empty;
     public string Status { get; set; } = string.Empty;
     public DateTime Created { get; set; }
     public DateTime Finished { get; set; }
