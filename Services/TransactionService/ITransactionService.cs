c45a0568c9bd9755a6bb21c629e03dffc98f9af4
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-11-28 16:16:54 +0200

    Atualizar o ITransactionService.cs

--- Services/TransactionService/ITransactionService.cs
+++ Services/TransactionService/ITransactionService.cs
@@ -1,1 +1,11 @@
-v
+using micropay.Dto;
+using micropay.Models;
+using micropay.ViewModels;
+
+namespace micropay.Services.TransactionService;
+
+public interface ITokenService
+{
+    Task<string> Create(TransactionDto transactionDto);
+    Task<List<TransactionViewModel>> GetTransactions(Token token);
+}
