dad7d0f9909702e67e01e85c4c760013006eb88e
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-10 20:31:32 +0200

    Atualizar o ITransactionService.cs

--- Services/TransactionService/ITransactionService.cs
+++ Services/TransactionService/ITransactionService.cs
@@ -9,5 +9,6 @@ public interface ITransactionService
     Task<string> Create(TransactionDto transactionDto);
     Task<List<TransactionViewModel>> GetTransactions(Token token);
     Task<string> Delete(Guid Id);
+    Task<string> Pay(Guid Id, string Provider);
     Task<string> PayDirect(Guid Id);
 }
