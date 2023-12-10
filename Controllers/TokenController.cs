6c1b12df7962610df71be9b51feb45fdf93e87c1
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-10 13:21:01 +0200

    Atualizar o TokenController.cs

--- Controllers/TokenController.cs
+++ Controllers/TokenController.cs
@@ -42,6 +42,14 @@ public class TokenController : ControllerBase
         return Ok(result);
     }
 
+    [HttpDelete, Authorize]
+    [Route("delete")]
+    public async Task<ActionResult<string>> Create([FromQuery] Id)
+    {
+        //var result = await _tokenService.Create(tokenDto);
+        return Ok("Apagado! ");
+    }
+
 
 }
 
