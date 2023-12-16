d2c672f4f66b6bf7624ed0693175a30f905da051
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-16 23:52:48 +0200

    Atualizar o BulkService.cs

--- Services/BulkService/BulkService.cs
+++ Services/BulkService/BulkService.cs
@@ -2,4 +2,12 @@ namespace micropay.Services.BulkService;
 
 public class BulkService : IBulkService
 {
+    private readonly IHttpContextAccessor _accessor;
+    private readonly IAuthService _authService;
+
+    public BulkService(IHttpContextAccessor accessor, IAuthService authService)
+    {
+        _accessor = accessor;
+        _authService = authService;
+    }
 }
