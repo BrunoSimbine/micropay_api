27da15a0fb47c2f4d80af8859e541f4ae4b87931
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-17 16:49:36 +0200

    Atualizar o UserDto.cs

--- Dto/UserDto.cs
+++ Dto/UserDto.cs
@@ -4,6 +4,5 @@ public class UserDto
 {
     public string Phone { get; set; } =  string.Empty;
     public string Name { get; set; } = string.Empty;
-    public string Surname { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
 }
