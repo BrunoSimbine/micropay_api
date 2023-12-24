6edd867f646c9388f38f5102bc49847c2e9362b2
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-24 12:21:53 +0200

    Atualizar o Withdraw.cs

--- Models/Withdraw.cs
+++ Models/Withdraw.cs
@@ -5,7 +5,6 @@ namespace micropay.Models;
 public class WithdrawTemplate
 {
     public Guid Id { get; set; }
-    public string Provider { get; set; }
     public double Total { get; set; }
     public bool Confirmed { get; set; }
     public List<WithdrawItem> Items;
