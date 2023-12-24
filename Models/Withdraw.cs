83dcd12981be57aea162875929c572b3c6799278
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-24 10:55:47 +0200

    Atualizar o Withdraw.cs

--- Models/Withdraw.cs
+++ Models/Withdraw.cs
@@ -1,1 +1,10 @@
-sh
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace micropay.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public double Total { get; set; }
+    public List<Transaction> transactions;
+}
