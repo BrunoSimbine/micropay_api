6a2cb1708023d0387103a697479758dc5e89cfc0
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-24 11:27:36 +0200

    Atualizar o WithdrawItem.cs

--- Models/WithdrawItem.cs
+++ Models/WithdrawItem.cs
@@ -1,1 +1,11 @@
-hhs
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace micropay.Models;
+
+public class WithdrawItem
+{
+    public string Name { get; set; }
+    public Guid TransactionId { get; set; } = string.Empty;
+    public double OriginalValue { get; set; }
+    public double Value { get; set; }
+}
