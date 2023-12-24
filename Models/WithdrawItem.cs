5ddcde9410a76f7521b2d0a97193ecaed7fbf078
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2023-12-24 12:08:22 +0200

    Atualizar o WithdrawItem.cs

--- Models/WithdrawItem.cs
+++ Models/WithdrawItem.cs
@@ -6,6 +6,6 @@ public class WithdrawItem
 {
     public string Name { get; set; }
     public int TransactionId { get; set; } 
-    public double OriginalValue { get; set; }
-    public double Value { get; set; }
+    public double OriginalAmount { get; set; }
+    public double Amount { get; set; }
 }
