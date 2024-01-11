6ce86718a8838db0f61d7c7e09ea0cdea3434f21
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2024-01-11 22:42:44 +0200

    Atualizar o IPaymentService.cs

--- Services/PaymentService/IPaymentService.cs
+++ Services/PaymentService/IPaymentService.cs
@@ -6,5 +6,12 @@ namespace micropay.Services.PaymentService;
 
 public interface IPaymentService
 {
-
+    Task<bool> PayWithMpesa(int TransactionId);
+    Task<bool> PayWithEmola(int TransactionId);
+    Task<bool> PayDirectWithMpesa(string Contact);
+    Task<bool> PayDirectWithEmola(string Contact);
+    Task<bool> PayWithCard();
+    Task<bool> PayWithBitcoin();
+    Task<bool> PayWithPaypal();
+    Task<bool> PayWithSIMO();
 }
