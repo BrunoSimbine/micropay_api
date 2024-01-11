e318e9b085ee76335c322d3c5b4451b4dd7f7e42
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2024-01-11 20:39:45 +0200

    Atualizar o Document.cs

--- Models/Document.cs
+++ Models/Document.cs
@@ -1,1 +1,10 @@
-bvxd
+namespace micropay.Models;
+
+public class Document
+{
+    public User User { get; set; }
+    public Guid UserId { get; set; }
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+}
