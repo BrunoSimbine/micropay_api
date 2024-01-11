65c1f8c721c5717c6714a991b45d451aaddf9896
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2024-01-11 22:46:38 +0200

    Atualizar o PaymentController.cs

--- Controllers/PaymentController.cs
+++ Controllers/PaymentController.cs
@@ -1,1 +1,39 @@
-ffg
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Security.Cryptography;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using micropay.Data;
+using micropay.Dto;
+using micropay.Models;
+using micropay.ViewModels;
+using micropay.Services.AuthService;
+using micropay.Services.TokenService;
+using micropay.Services.TransactionService;
+
+namespace micropay.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PaymentController : ControllerBase
+{
+    private readonly DataContext _context;
+    private readonly IPaymentService _paymentService;
+    public PaymentController(DataContext context, IPaymentService paymentService)
+    {
+        _paymentService = paymentService;
+        _context = context;
+    }
+
+    [HttpGet, Authorize]
+    [Route("get")]
+    public async Task<ActionResult<TransactionViewModel>> GetAll([FromQuery] Guid tokenId)
+    {
+        return Ok();
+    }
+
+
+
+}
