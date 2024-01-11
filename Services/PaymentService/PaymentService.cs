f46f526482f63170e83ca460b1063daa4eb086ee
Bruno Simbine <60631114+BrunoSimbine@users.noreply.github.com>
2024-01-11 22:37:20 +0200

    Atualizar o PaymentService.cs

--- Services/PaymentService/PaymentService.cs
+++ Services/PaymentService/PaymentService.cs
@@ -6,41 +6,43 @@ public class PaymentService : IPaymentService
 {
     public async Task<bool> PayWithMpesa(int TransactionId)
     {
-        
+        return true;
     }
 
     public async Task<bool> PayWithEmola(int TransactionId)
     {
-        
+        return true;
     }
 
     public async Task<bool> PayDirectWithMpesa(string Contact)
     {
-        
+        await Task.Delay(2000);
+        return true;
     }
 
     public async Task<bool> PayDirectWithEmola(string Contact)
     {
-        
+        await Task.Delay(2000);
+        return true;
     }
 
     public async Task<bool> PayWithCard()
     {
-        
+        return true;
     }
 
     public async Task<bool> PayWithBitcoin()
     {
-        
+        return true;
     }
 
     public async Task<bool> PayWithPaypal()
     {
-        
+        return true;
     }
 
     public async Task<bool> PayWithSIMO()
     {
-        
+        return true;
     }
 }
